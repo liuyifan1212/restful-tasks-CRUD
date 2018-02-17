7c0dd019b30bca9a9bf62b323be7be68bf42dd6d
liuyifan1212 <32651077+liuyifan1212@users.noreply.github.com>
2018-02-17 12:03:17 -0800

    Update app.component.ts

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { HttpService } from './http.service';
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'MEAN';
-  constructor(private _httpService: HttpService){}
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class HttpService {
+
+  constructor(private _http: HttpClient) {
+    //this.getTasks();
+    //this.getOne();
+   }
+
+  getTasks(){
+    //let tempObservable = this._http.get('/tasks');
+    //tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    return this._http.get('/tasks')
+ }
 }
