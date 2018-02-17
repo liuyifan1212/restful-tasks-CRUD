dacd7955f47d37d478a2e6c8684fa26cd0001526
liuyifan1212 <32651077+liuyifan1212@users.noreply.github.com>
2018-02-17 14:48:36 -0800

    Update app.component.ts

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -7,27 +7,29 @@ import { HttpService } from './http.service';
 })
 export class AppComponent implements OnInit {
   title = 'MEAN';
-  tasks = [];
-  //a : string
-  b = [];
+  data : [string];
+  b: string;
+  c: string;
+  d: [string];
+  e: string
+
+  onButtonClick(): void { 
+    console.log(`Click event is working`);
+    this.d = this.data
+  }
 
   constructor(private _httpService: HttpService){}
   ngOnInit(){
     this.getTaskFromService()
-    
   }
   getTaskFromService(){
     let observable = this._httpService.getTasks()
     observable.subscribe(data =>{
       //console.log("Congrats!got your data!", data)
-      this.tasks = data['tasks'];
-     // console.log(data.data)
-      this.b = data.data
-      //console.log(data.data[0].description)
-     // this.a = data.data[0].description
+      this.data = data['data'];
+      this.b = data['data'][2].title
+      this.c = data['data'][2].description
     
     })
   }
- 
 }
-
