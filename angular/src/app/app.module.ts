823e7e357c81bb6695982614cea2f6f49fd1347a
liuyifan1212 <32651077+liuyifan1212@users.noreply.github.com>
2018-02-19 18:57:27 -0800

    Update app.module.ts

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { HttpService } from './http.service';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [HttpService],
