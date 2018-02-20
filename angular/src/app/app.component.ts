159e44b5460e826b8ab7eb449cbdc976e1a44cdb
liuyifan1212 <32651077+liuyifan1212@users.noreply.github.com>
2018-02-19 18:58:51 -0800

    Update app.component.ts

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -8,28 +8,54 @@ import { HttpService } from './http.service';
 export class AppComponent implements OnInit {
   title = 'MEAN';
   data : [string];
-  b: string;
-  c: string;
   d: [string];
-  e: string
-
-  onButtonClick(): void { 
-    console.log(`Click event is working`);
-    this.d = this.data
-  }
+  newTask: any;
+  newTask2: any;
+  
 
   constructor(private _httpService: HttpService){}
   ngOnInit(){
     this.getTaskFromService()
+    this.newTask = { title: "", description: "" }
   }
+  onSubmit(){
+    let observable = this._httpService.addTask(this.newTask);
+    observable.subscribe(data => {
+      console.log("Got data from back", data);
+      this.newTask = {title: '',description:''}
+      this.getTaskFromService()
+    })
+  }
+  updateSubmit(){
+      let id = this.newTask2['id'];
+      this._httpService.updateTask(id,this.newTask2).subscribe(data=>{
+        console.log(data);
+        this.newTask2={title:"",description:""}
+        this.newTask2 = null
+        this.getTaskFromService()
+      })
+
+  }
+  deleteTask(id){
+    this._httpService.removeTask(id).subscribe(data=>{
+      console.log(data)
+      this.data = data['data'];
+      this.getTaskFromService()
+    });    
+  }
+  updateTask(i){
+    this.newTask2 = {id:i._id,title:i.title,description:i.description}
+   
+  }
+
+
   getTaskFromService(){
     let observable = this._httpService.getTasks()
     observable.subscribe(data =>{
       //console.log("Congrats!got your data!", data)
       this.data = data['data'];
-      this.b = data['data'][2].title
-      this.c = data['data'][2].description
-    
+      
     })
   }
 }
+
