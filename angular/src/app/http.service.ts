970ca6ae1ff47854f6068b14b68bc1ff8bf04ee0
liuyifan1212 <32651077+liuyifan1212@users.noreply.github.com>
2018-02-19 18:57:55 -0800

    Update http.service.ts

--- angular/src/app/http.service.ts
+++ angular/src/app/http.service.ts
@@ -7,36 +7,20 @@ export class HttpService {
   constructor(private _http: HttpClient) {
     //this.getTasks();
     //this.getOne();
-    this.getPokemon();
-    this.a();
-    this.b();
    }
 
   getTasks(){
-    let tempObservable = this._http.get('/tasks');
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
- }
-  getOne(){
-    let tempObservable = this._http.get('/tasks/5a84e9f75534fefbed1b1caf');
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    //let tempObservable = this._http.get('/tasks');
+    //tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    return this._http.get('/tasks')
   }
-  getPokemon(){
-    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
-    bulbasaur.subscribe(data => console.log("Bulbasaur's abilities are ",data.abilities[0].ability.name,"and",data.abilities[1].ability.name ));
-    
+  addTask(newtask){
+    return this._http.post('/tasks', newtask)
   }
-  a(){
-    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/ability/65/');
-    bulbasaur.subscribe(data => console.log(data.pokemon.length+" Pokemon have the overgrow ability!" ));
+  removeTask(id){
+    return this._http.delete('/tasks/'+id);
   }
-  b(){
-    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/ability/65/');
-    bulbasaur.subscribe(function(data){
-      for(var i in data.pokemon){
-        console.log(data.pokemon[i].pokemon.name )
-      }
-    });
+  updateTask(id,newTask2){
+    return this._http.put('/tasks/'+id,newTask2);
   }
-
 }
-
